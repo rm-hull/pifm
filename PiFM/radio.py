f5876aa802134b6065397e2397274118bde3e07b
James Utter <james.utter@gmail.com>
2014-05-06 22:39:33 +1000

    Corrected path to pifm binary

--- PiFM/radio.py
+++ PiFM/radio.py
@@ -4,8 +4,12 @@ from subprocess import call
 
 
 def play_sound( filename, carrier_freq=99.9, sample_rate=22050, stereo=False):
+    """Broadcast wav file `filename` using pifm for Raspberry Pi.
+
+    Radio is tuned to `carrier_freq` (MHz)
+    """
     if stereo:
-        call(["./pifm", filename, carrier_freq, sample_rate, "stereo"])
+        call(["pifm", filename, "{}".format(carrier_freq), "{}".format(sample_rate), "stereo"])
     else:
-        call(["./pifm", filename, carrier_freq, sample_rate])
+        call(["pifm", filename, "{}".format(carrier_freq), "{}".format(sample_rate)])
     return
